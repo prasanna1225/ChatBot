809208e1237ccddc9884df83d120294e54716c77
Prasanna <prasannasparkle1225@gmail.com>
2026-08-01 18:37:29 +0530

    code for chatbot

--- chatWithMe.py
+++ chatWithMe.py
@@ -1,10 +1,10 @@
 from google import genai
 import os
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
-response=client.models.generate_content_stream(
-    model="gemini-3.5-flash",
-    contents="Why trees are green?"
-  
-)
-for stream in response:
-    print(stream.text)
+chat=client.chats.create(model="gemini-3.5-flash")
+while True:
+    message = input("> ")
+    if message == "exit":
+        break
+    res=chat.send_message(message)
+    print(res.text)
